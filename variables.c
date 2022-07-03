ef9ea69aa677560b2839d017707f297149fe469f
IsraelR20 <israelrojasvilca@gmail.com>
2022-07-03 10:57:24 -0500

    variables

---
+++ variables.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+int main (){
+    printf("\nVariables y tipos de datos\n");
+    int enteroA=1;
+    float flotanteA=1.1;
+    double doubleA=1.1;
+    char caracterA="A";
+    printf("El valor del enteroA es: %i\n",enteroA);
+    printf("El valor del flotanteA es: %f\n",flotanteA);
+    return 0;   
+}
