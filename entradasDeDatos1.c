6d293f441624a0e5fe0bdcbb2f7baaeaf688a722
IsraelR20 <israelrojasvilca@gmail.com>
2022-07-03 11:26:00 -0500

    Salida de datos 1

---
+++ entradasDeDatos1.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+int main(){
+    printf("\nSalida de datos\n");
+    int enteroA=1;
+    float flotanteA=1.1;
+    double doubleA=1.1;
+    char caracterA='S';
+    printf("El valor del enteroA es: %i\n",enteroA);
+    printf("El valor del flotanteA es: %f\n",flotanteA);
+    printf("El valor del doubleA es: %f\n",doubleA);
+    printf("El valor del caracterA es: %c\n",caracterA);
+    return 0;
+}
